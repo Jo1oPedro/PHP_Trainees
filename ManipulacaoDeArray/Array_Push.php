cedccdc37c36d84ad7888741b5eef6176c9e0bc2
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-03-25 03:38:58 -0300

    adiciona funcao array_unshift

--- ManipulacaoDeArray/Array_Push.php
+++ ManipulacaoDeArray/Array_Push.php
@@ -4,4 +4,4 @@ $nomes = ['Cascata', 'CascataShounen', 'CascataXphp'];
 
 $newNome = 'CascataVerso';
 
-$array_push($nomes, $newNome); // adiciona um novo elemento ao array $nomes;
+$array_push($nomes, $newNome); // adiciona um novo elemento ao final do array $nomes;
---
+++ ManipulacaoDeArray/Array_unshift.php
@@ -0,0 +1,7 @@
+<?php
+
+$nomes = ['Cascata', 'CascataShounen', 'CascataXphp'];
+
+$newNome = ['CascataSearchLove'];
+
+$array_unshift($nomes, $newNome); // adiciona um novo elemento no inicio do array $nomes
